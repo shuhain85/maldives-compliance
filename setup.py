0421fa39df6361c5889ea87582892c875deadc6f
Shuhain Ismail <shuhain.ismail@gmail.com>
2025-05-31 06:12:35 +0000

    update

--- setup.py
+++ setup.py
@@ -2,14 +2,4 @@ from setuptools import setup
 
 name = "maldives_compliance"
 
-setup(
-    name="maldives_compliance",
-    version="1.0.0",
-    description="Compliance validations and tax structure for Maldives GST (8%) and TGST (17%)",
-    author="shuhain85",
-    author_email="shuhain.ismail@gmail.com",
-    packages=find_packages(include=['maldives_compliance', 'maldives_compliance.*']),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[],
-)
+setup()
